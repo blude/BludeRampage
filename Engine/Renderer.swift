db844c90ca7c313080b56f95160279c5d6debc9b
Saulo Pratti <blude@me.com>
2020-05-10 01:59:44 +0200

    Add draw2D as a fallback for debugging

--- Engine/Renderer.swift
+++ Engine/Renderer.swift
@@ -17,6 +17,52 @@ public struct Renderer {
 }
 
 public extension Renderer {
+    mutating func draw2D(_ world: World) {
+        let scale = Double(bitmap.height) / world.size.y
+
+        // Draw map
+        for y in 0 ..< world.map.height {
+            for x in 0 ..< world.map.width where world.map[x, y].isWall {
+                let rect = Rect(
+                    min: Vector(x: Double(x), y: Double(y)) * scale,
+                    max: Vector(x: Double(x + 1), y: Double(y + 1)) * scale
+                )
+                bitmap.fill(rect: rect, color: .white)
+            }
+        }
+
+        // Draw player
+        var rect = world.player.rect
+        rect.min *= scale
+        rect.max *= scale
+        bitmap.fill(rect: rect, color: .blue)
+
+        // Draw view plane
+        let focalLength = 1.0
+        let viewWidth = 1.0
+        let viewPlane = world.player.direction.orthogonal * viewWidth
+        let viewCenter = world.player.position + world.player.direction * focalLength
+        let viewStart = viewCenter - viewPlane / 2
+        let viewEnd = viewStart + viewPlane
+        bitmap.drawLine(from: viewStart * scale, to: viewEnd * scale, color: .red)
+
+        // Cast rays
+        let columns = 10
+        let step = viewPlane / Double(columns)
+        var columnPosition = viewStart
+        for _ in 0 ..< columns {
+            let rayDirection = columnPosition - world.player.position
+            let viewPlaneDistance = rayDirection.length
+            let ray = Ray(
+                origin: world.player.position,
+                direction: rayDirection / viewPlaneDistance
+            )
+            let end = world.map.hitTest(ray)
+            bitmap.drawLine(from: ray.origin * scale, to: end * scale, color: .green)
+            columnPosition += step
+        }
+    }
+    
     mutating func draw(_ world: World) {
         /**
          The length of the line represents the view width in world units. This has no direct relationship
