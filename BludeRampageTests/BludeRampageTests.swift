672b131bbf29bea4b06b192ac4240639c5414599
Saulo Pratti <blude@me.com>
2020-05-13 00:57:14 +0200

    Add simple test case

--- BludeRampageTests/BludeRampageTests.swift
+++ BludeRampageTests/BludeRampageTests.swift
@@ -7,26 +7,22 @@
 //
 
 import XCTest
+import Engine
+import BludeRampage
 
 class BludeRampageTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    let world = World(map: loadMap())
+    let textures = loadTextures()
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
+    func testRenderFrame() {
         measure {
-            // Put the code you want to measure the time of here.
+            var renderer = Renderer(width: 1000, height: 1000, textures: textures)
+            renderer.draw(world)
         }
     }
 
