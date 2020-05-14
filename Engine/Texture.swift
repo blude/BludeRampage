8b06a293af09206090e291aa13d3e4daa02e6048
Saulo Pratti <blude@me.com>
2020-05-14 02:21:59 +0200

    Add doorjamb textures

--- Engine/Texture.swift
+++ Engine/Texture.swift
@@ -10,7 +10,7 @@ public enum Texture: String, CaseIterable {
     case wall, crackWall, slimeWall
     case wall2, crackWall2, slimeWall2
     case floor, crackFloor
-    case door, door2
+    case door, door2, doorjamb, doorjamb2
     case ceiling
     case monster, monsterWalk1, monsterWalk2
     case monsterHurt, monsterDeath1, monsterDeath2, monsterDead
