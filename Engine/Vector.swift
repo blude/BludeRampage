cc0a2f19e1932ac1c0ef7ec2ebe4359fd01d41ab
Saulo Pratti <blude@me.com>
2020-05-15 00:22:37 +0200

    Misc code formatting

--- Engine/Vector.swift
+++ Engine/Vector.swift
@@ -32,37 +32,46 @@ public extension Vector {
     static func + (lhs: Vector, rhs: Vector) -> Vector {
         Vector(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
     }
+    
     static func - (lhs: Vector, rhs: Vector) -> Vector {
         Vector(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
     }
+    
     static func * (lhs: Vector, rhs: Double) -> Vector {
         Vector(x: lhs.x * rhs, y: lhs.y * rhs)
     }
+    
     static func / (lhs: Vector, rhs: Double) -> Vector {
         Vector(x: lhs.x / rhs, y: lhs.y / rhs)
     }
+    
     static func * (lhs: Double, rhs: Vector) -> Vector {
         Vector(x: lhs * rhs.x, y: lhs * rhs.y)
     }
     static func / (lhs: Double, rhs: Vector) -> Vector {
         Vector(x: lhs / rhs.x, y: lhs / rhs.y)
     }
+    
     static func += (lhs: inout Vector, rhs: Vector) {
         lhs.x += rhs.x
         lhs.y += rhs.y
     }
+    
     static func -= (lhs: inout Vector, rhs: Vector) {
         lhs.x -= rhs.x
         lhs.y -= rhs.y
     }
+    
     static func *= (lhs: inout Vector, rhs: Double) {
         lhs.x *= rhs
         lhs.y *= rhs
     }
+    
     static func /= (lhs: inout Vector, rhs: Double) {
         lhs.x /= rhs
         lhs.y /= rhs
     }
+    
     static prefix func - (rhs: Vector) -> Vector {
         Vector(x: -rhs.x, y: -rhs.y)
     }
