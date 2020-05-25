68d688b26fc6e48ecfae5e4c130ec5a622eefaed
Saulo Pratti <blude@me.com>
2020-05-25 11:53:13 +0200

    Make texture conform to Decodable protocol

--- Engine/Texture.swift
+++ Engine/Texture.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Pratti Design. All rights reserved.
 //
 
-public enum Texture: String, CaseIterable {
+public enum Texture: String, CaseIterable, Decodable {
     case wall, wall2
     case crackWall, crackWall2
     case slimeWall, slimeWall2
